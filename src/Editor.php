cd93ab945d6069f9c9f058049e23fc012187f70a
manmohan <mmsingh62@gmail.com>
2025-03-24 10:46:27 +0530

    resources/assets

--- src/Editor.php
+++ src/Editor.php
@@ -9,7 +9,13 @@ class Editor extends Textarea
     protected $view = 'laravel-admin-ckeditor::editor';
 
     protected static $js = [
-        'vendor/laravel-admin-ckeditor/ckeditor.js',
+        // 'vendor/laravel-admin-ckeditor/ckeditor.js',
+        'https://cdn.ckeditor.com/ckeditor5/44.3.0/ckeditor5.umd.js',
+        // 'vendor/laravel-admin-ckeditor/customckeditor.js',
+    ];
+
+    protected static $css = [
+        'https://cdn.ckeditor.com/ckeditor5/44.3.0/ckeditor5.css',
     ];
 
     public function render()
@@ -17,10 +23,31 @@ class Editor extends Textarea
         $config = (array) CKEditor::config('config');
 
         $config = json_encode(array_merge($config, $this->options));
+        $ckeditorKey = env('CKEDITOR_KEY', '');
 
         $this->script = <<<EOT
-CKEDITOR.replace('{$this->id}', $config);
+const {
+    ClassicEditor,
+    Essentials,
+    Bold,
+    Italic,
+    Font,
+    Paragraph
+} = CKEDITOR;
+
+ClassicEditor
+        .create( document.querySelector( '#{$this->id}' ), {
+            licenseKey: '$ckeditorKey',
+            plugins: [ Essentials, Bold, Italic, Font, Paragraph ],
+            toolbar: [
+                'undo', 'redo', '|', 'bold', 'italic', '|',
+                'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor'
+            ]
+        } )
+        .then( )
+        .catch( );
 EOT;
         return parent::render();
     }
 }
+
